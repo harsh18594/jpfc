35434315b82ebc04b691df7ce2f6e28fed361a85
Harsh Dave <harsh18594@gmail.com>
2019-03-25 21:05:51 -0400

    committing progress on template setup

---
+++ jpfc/wwwroot/js/pages/services-buysellgold.js
@@ -0,0 +1,28 @@
+﻿var Jpfc = Jpfc || {};
+
+Jpfc.ServicesBuySellGold = function () {
+    var viewModel;
+
+    var initDataTable = function () {
+        $('#price-table').DataTable({
+            dom: 'Bfrtip',
+            "paging": false,
+            "info": true,
+            "searching": false,
+            buttons: [
+                //'copyHtml5',
+                'excelHtml5',
+                //'csvHtml5',
+                'pdfHtml5'
+            ]
+        });
+    };
+
+    var init = function () {
+        initDataTable();
+    };
+
+    return {
+        init: init
+    };
+}();
