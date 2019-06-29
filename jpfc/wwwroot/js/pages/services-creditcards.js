c31b4397d105757e2cabf7ca4051ad8b2601d2ea
Harsh Dave <harsh18594@gmail.com>
2019-06-29 17:33:15 -0400

    site content update

---
+++ jpfc/wwwroot/js/pages/services-creditcards.js
@@ -0,0 +1,15 @@
+﻿var Jpfc = Jpfc || {};
+
+Jpfc.ServicesCreditCards = function () {
+    var initFancyBox = function () {
+        $('a.image').fancybox();
+    };
+
+    var init = function () {
+        initFancyBox();
+    };
+
+    return {
+        init: init
+    };
+}();
